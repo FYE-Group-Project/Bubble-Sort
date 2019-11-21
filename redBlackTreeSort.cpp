b636eb7012c6f84027f59d6d106759079aa6474e
19smusselman <musselmanseth@gmail.com>
2019-11-20 19:21:05 -0500

    main and comments added

---
+++ redBlackTreeSort.cpp
@@ -0,0 +1,10 @@
+// Bubble sort Algorithm using Red Black Tree
+// A red–black tree is a kind of self-balancing binary search tree
+// Each node of the binary tree has an extra bit, and that bit is often interpreted as the color of the node
+// These color bits are used to ensure the tree remains approximately balanced during insertions and deletions
+// 11/20/19
+// Seth Musselman, Avery Miller, Evan Oleary, Jeremy Martin, James Meyer, Tre' Moore
+
+int main() {
+
+}
